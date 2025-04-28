1c3175f81854a69387be56ac0375e57531a0b537
marconav2000 <151166014+marconav2000@users.noreply.github.com>
2025-04-28 14:46:43 -0400

    Agregando estilos al menu de navegacion en archivo Style

--- app/public/wp-content/themes/gymmalilla/footer.php
+++ app/public/wp-content/themes/gymmalilla/footer.php
@@ -1,6 +1,11 @@
 
 <footer class="site-footer">
             <p>Site Footer &copy; MALILLAGYM. All rights reserved.</p>
+</footer>   
+
+<?php wp_footer(); ?>
+ <!-- Fin del contenedor principal -->
+
 
     </body>
 </html>
