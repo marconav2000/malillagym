9492366bbc06df73020447f8a7c04edde47f8c5b
marconav2000 <151166014+marconav2000@users.noreply.github.com>
2025-04-29 16:39:25 -0400

    dando estilos al footer

--- app/public/wp-content/themes/gymmalilla/footer.php
+++ app/public/wp-content/themes/gymmalilla/footer.php
@@ -1,6 +1,19 @@
 
-<footer class="site-footer">
-            <p>Site Footer &copy; MALILLAGYM. All rights reserved.</p>
+<footer class="site-footer contenedor">
+    <hr>
+
+        <div class="contenido-footer">
+            <?php 
+                $argc = array(
+                    'theme_location' => 'menu-principal',
+                    'container'      => 'nav',
+                    'container_class'=> 'menu-principal',                   
+                );
+                wp_nav_menu( $argc );
+            ?>
+            <p class="copyright">Todos los derechos reservados &copy; <?php echo get_bloginfo('name') . " " . date('Y') ?></p>
+
+        </div>    
 </footer>   
 
 <?php wp_footer(); ?>
--- app/public/wp-content/themes/gymmalilla/js/scripts.js
+++ app/public/wp-content/themes/gymmalilla/js/scripts.js
@@ -1,5 +1,5 @@
 jQuery(document).ready( $ =>  {
-    $('.menu-principal .menu').slicknav({
+    $('.site-header .menu-principal .menu').slicknav({
         // label: '', // muestra lo que va entre las '', si no usamos la eqtiqueta carga por defecto MENU
         appendTo: '.site-header',
       
