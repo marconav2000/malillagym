4ef934b01ee90a2459707efbdd7e6f1d29ee7617
marconav2000 <151166014+marconav2000@users.noreply.github.com>
2025-04-30 14:59:15 -0400

    creando archivo page-sin-sidebar.php

---
+++ app/public/wp-content/themes/gymmalilla/page-sin-sidebar.php
@@ -0,0 +1,39 @@
+<!-- carga contenido de archivo header.php -->
+<?php 
+/*
+* Teplate Name: contenido creadio (Sin Sidebar)
+*/
+
+get_header(); ?>
+
+    <main class="contenedor pagina seccion no-sidebar">
+        <div class="contenido-principal">
+            <!-- Carga contenido de la pagina a publicar -->
+            <?php while ( have_posts() ) : the_post(); ?>
+    
+                <!-- Carga titulo  de la pagina a publicar -->
+                <h1 class="text-center texto-primario"><?php the_title(); ?></h1>
+ 
+                <!-- Carga imagen despuesd e titulo -->
+                <?php 
+                    if( has_post_thumbnail() ) :
+                            the_post_thumbnail('blog', array('class' => 'imagen-destacada'));
+                        
+                    // else:   
+ 
+                    //     echo '<img src="' . get_template_directory_uri() . '/img/imagen-default.jpg" alt="Imagen por defecto">';
+                    endif;     
+                ?>
+                <!-- Carga cuerpo (contenido) de publicacion  -->
+                <?php the_content(); ?>    
+ 
+            <?php endwhile; ?>
+                    
+        
+
+    </main>    
+
+
+
+<!-- carga contenido de archivo footert.php -->
+<?php get_footer(); ?>
