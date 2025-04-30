a76f39999aa848210026b8de134be0029f467fa3
marconav2000 <151166014+marconav2000@users.noreply.github.com>
2025-04-30 16:42:18 -0400

    creando un Slug para nuestras clases

---
+++ app/public/wp-content/themes/gymmalilla/page-nuestras-clases.php
@@ -0,0 +1,19 @@
+<!-- carga contenido de archivo header.php -->
+<?php get_header(); ?>
+
+    <main class="contenedor pagina seccion no-sidebar">
+        <div class="contenido-principal text-center">
+            
+            <!-- carga contenido de pagina.php -->
+            <?php get_template_part('template-parts/paginas'); ?>
+
+                    
+        </div>
+      
+
+    </main>    
+
+
+
+<!-- carga contenido de archivo footert.php -->
+<?php get_footer(); ?>
