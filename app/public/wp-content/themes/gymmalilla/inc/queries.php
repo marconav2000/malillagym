0ea22f9f23304df7de68abd8d079559fd0730043
marconav2000 <151166014+marconav2000@users.noreply.github.com>
2025-05-06 15:30:54 -0400

    agregando estilos a tarjetas para version desktop

--- app/public/wp-content/themes/gymmalilla/inc/queries.php
+++ app/public/wp-content/themes/gymmalilla/inc/queries.php
@@ -17,7 +17,7 @@ function gymmalilla_lista_clases() { ?>
        
                 while ($clases->have_posts()): $clases->the_post(); ?>
                    
-                <li class="clase card">
+                <li class="clase card gradient">
                     <?php the_post_thumbnail('mediana'); ?>
                     <div class="contenido">
                         <a href="<?php the_permalink(); ?>">
