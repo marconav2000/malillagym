e7ecf919f79a4365d59655744f276331b630b28a
marconav2000 <151166014+marconav2000@users.noreply.github.com>
2025-05-20 08:13:30 -0400

    programando archivo home.php

--- app/public/wp-content/themes/gymmalilla/home.php
+++ app/public/wp-content/themes/gymmalilla/home.php
@@ -1,6 +1,15 @@
 <?php get_header(); ?>
 
-
+    <main class="pagina seccion no-sidebar contenedor">
+        <ul class="listado-blog">
+            <?php while ( have_posts() ) : the_post(); ?>
+                <li class="card gradient">
+                   <?php the_post_thumbnail('mediana'); ?>
+                    
+                </li>
+            <?php endwhile; ?>
+        </ul>
+    </main>
 
 
 <?php get_footer(); ?>
